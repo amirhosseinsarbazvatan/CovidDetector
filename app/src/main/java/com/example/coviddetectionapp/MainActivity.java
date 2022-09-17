fbd20fcb3a51d1402fb0e0c05b55cb58709ef68d
Amirhossein <amirhosseinsarbazvatan@gmail.com>
2022-09-17 17:44:29 +0430

    first commit

---
+++ app/src/main/java/com/example/coviddetectionapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.coviddetectionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread splashTime = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                }catch (Exception e){
+
+                }finally {
+                    Intent firstPage = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(firstPage);
+                }
+            }
+        };
+        splashTime.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
